53a08c37b089bac726a7ee365bfcccba3ec98ab0
pguerra98 <119546662+pguerra98@users.noreply.github.com>
2023-05-12 20:07:31 -0400

    added in some code to the files that had TO-DOs

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -19,10 +19,82 @@ module.exports = () => {
     },
     plugins: [
       
+      new HtmlWebpackPlugin({template: './index.html', title: 'JATE'}),
+
+      new InjectManifest({
+
+        swSrc: './src-sw.js',
+
+        swDest: 'src-sw.js',
+
+      }),
+
+      new WebpackPwaManifest ({
+
+        fingerprints: false,
+
+        inject: true,
+
+        name: 'text-editor',
+
+        short_name: 'JATE',
+
+        description: 'My Text Editor',
+
+        start_url: '/',
+
+        publicPath: '/',
+
+        icons: [
+
+          {
+
+            src: path.resolve('src/images/logo.png'),
+
+            sizes: [96,128,192,256,384,512],
+
+            destination: path.join('assets', 'icons'),
+
+          },
+
+        ],
+
+      }),
+
     ],
 
     module: {
+
       rules: [
+
+        {
+
+          test: /\.css$/i,
+          
+          use: ['style-loader', 'css-loader'],
+
+        },
+
+        {
+
+          test: /\.m?js$/,
+          
+          exclude: /node_modules/,
+
+          use: {
+
+            loader: 'babel-loader',
+
+            options: {
+
+              presets: ['@babel/preset-env'],
+
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+
+            },
+          }
+
+        }
         
       ],
     },
